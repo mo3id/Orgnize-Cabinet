4e85c5de45bfb6443ee46dad05cd903a9e07be03
Ahmed Alhosary <ahmedalhosary67@gmail.com>
2021-11-08 00:21:04 +0200

    update 1

--- js/main.js
+++ js/main.js
@@ -2,9 +2,16 @@
 //   document.getElementById("popup-1").classList.toggle("active");
 // }
 
-$(".Click-here").on('click', function() {
-  $(".custom-model-main").addClass('model-open');
-}); 
+// $(".Click-here").on('click', function() {
+//   $(".custom-model-main").addClass('model-open');
+// }); 
 $(".close-btn, .bg-overlay").click(function(){
   $(".custom-model-main").removeClass('model-open');
 });
+
+let itemButton = `<button class="Click-here">+ Item</button>`;
+// let answerDiv = `<span></span>`
+
+chooseClothes = () => {
+  $(".custom-model-main").addClass('model-open');
+}
